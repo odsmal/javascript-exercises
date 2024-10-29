524ab7b8c616b9580dea7eaa7432433f21041537
odsmal <44933047+odsmal@users.noreply.github.com>
2024-10-29 18:02:33 +0100

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (lst, ...value) {
+  console.log(lst);
+  console.log(value);
+
+  let filtered = lst;
+
+  for (let j = 0; j < value.length; j++) {
+    filtered = filtered.filter((item) => item !== value[j]);
+  }
+
+  return filtered;
 };
 
 // Do not edit below this line
