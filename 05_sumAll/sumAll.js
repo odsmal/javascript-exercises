c46275cba41c78091f5efa0a337d429303179137
odsmal <44933047+odsmal@users.noreply.github.com>
2024-10-29 18:53:29 +0100

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (!Number.isInteger(x) || x < 0) return "ERROR";
+  if (!Number.isInteger(y) || y < 0) return "ERROR";
 
+  let smallest = 0;
+  let largest = 0;
+  let sum = 0;
+
+  if (x < y) {
+    smallest = x;
+    largest = y;
+  } else {
+    smallest = y;
+    largest = x;
+  }
+
+  for (let i = smallest; i < largest + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
