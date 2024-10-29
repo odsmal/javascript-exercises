30e304d12979149249b8fbcd0d1ecd743fa51776
odsmal <44933047+odsmal@users.noreply.github.com>
2024-10-29 13:02:42 +0100

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (s) {
+  let reversed = "";
 
+  for (let i = 0; i < s.length; i++) {
+    reversed += s.charAt(s.length - 1 - i);
+  }
+  return reversed;
 };
 
 // Do not edit below this line
