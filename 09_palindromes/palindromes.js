2843a76ee96da582d1ee9f39f2cde8efcdd873c3
odsmal <44933047+odsmal@users.noreply.github.com>
2024-11-21 22:02:34 +0100

    9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function (string) {
-  const array = string.split("");
+const palindromes = function (str) {
+  const array = str
+    .toLowerCase()
+    .split("")
+    .filter((item) => item.match(/\d+|[a-zA-Z]+/g));
   for (let i = 0; i < Math.floor(array.length / 2); i++) {
     if (array[i] !== array[array.length - i - 1]) return false;
   }
