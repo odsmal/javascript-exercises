963d29e225e1e4f171acb5e5ed15eb08eee97f59
odsmal <44933047+odsmal@users.noreply.github.com>
2024-11-25 21:17:32 +0100

    should've used map

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  let arr = [];
+  for (const book in books) {
+    arr.push(books[book].title);
+  }
+  return arr;
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
